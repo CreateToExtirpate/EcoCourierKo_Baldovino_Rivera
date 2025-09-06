ee1292f3254d28f6e12c39b50f87478ec904a8fa
Baldovino-Sean <sbaldovino_250000002235@uic.edu.ph>
2025-09-07 02:17:41 +0800

    Push CourierPage [ html, css, js ]

---
+++ function.js
@@ -0,0 +1,79 @@
+
+
+
+
+//  DIALOGS
+
+const dialog1 =document.querySelector("#dialog1");
+document.querySelector("#open-popup1").addEventListener("click", function(){
+    dialog1.showModal();
+});
+dialog1.querySelector(".close-btn").addEventListener("click", function(){
+    dialog1.close();
+});
+
+//dialog 2 v
+
+const dialog2 =document.querySelector("#dialog2");
+document.querySelector("#open-popup2").addEventListener("click", function(){
+    dialog2.showModal();
+});
+dialog2.querySelector(".close-btn").addEventListener("click", function(){
+    dialog2.close();
+});
+
+//dialog 3 v
+
+const dialog3 =document.querySelector("#dialog3");
+document.querySelector("#open-popup3").addEventListener("click", function(){
+    dialog3.showModal();
+});
+dialog3.querySelector(".close-btn").addEventListener("click", function(){
+    dialog3.close();
+});
+
+//dialog 4 v
+
+const dialog4 =document.querySelector("#dialog4");
+document.querySelector("#open-popup4").addEventListener("click", function(){
+    dialog4.showModal();
+});
+dialog4.querySelector(".close-btn").addEventListener("click", function(){
+    dialog4.close();
+});
+//dialog 5 v
+
+const dialog5 =document.querySelector("#dialog5");
+document.querySelector("#open-popup5").addEventListener("click", function(){
+    dialog5.showModal();
+});
+dialog5.querySelector(".close-btn").addEventListener("click", function(){
+    dialog5.close();
+});
+//dialog 6 v
+
+const dialog6 =document.querySelector("#dialog6");
+document.querySelector("#open-popup6").addEventListener("click", function(){
+    dialog6.showModal();
+});
+dialog6.querySelector(".close-btn").addEventListener("click", function(){
+    dialog6.close();
+});
+//dialog 7 v
+
+const dialog7 =document.querySelector("#dialog7");
+document.querySelector("#open-popup7").addEventListener("click", function(){
+    dialog7.showModal();
+});
+dialog7.querySelector(".close-btn").addEventListener("click", function(){
+    dialog7.close();
+});
+//dialog 8 v
+
+const dialog8 =document.querySelector("#dialog8");
+document.querySelector("#open-popup8").addEventListener("click", function(){
+    dialog8.showModal();
+});
+dialog8.querySelector(".close-btn").addEventListener("click", function(){
+    dialog8.close();
+});
